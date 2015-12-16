472c6f43ad0471b3266c69c604b954c344fdda7c
MishaRomodin <mikhail.romodin@phystech.edu>
2015-12-17 01:10:26 +0300

    Update usless.c

--- usless.c
+++ usless.c
@@ -15,35 +15,40 @@ int MaxStringSize = 100;
 int useless(char *str){
     int pause;
     char *argument=malloc(MaxStringSize);
-    char *command=malloc(MaxStringSize),*p;//выделяем еще память с p
+    char *command=malloc(MaxStringSize),*p;
     pid_t pid;	
-    if((pid=fork())<0){
+    if((pid=fork())<0)//проверяем возможность создания процесса ребенок
+    {
 	   printf("%s\n","Can't create child process.");
 	   exit(-1);
     }
     if (pid>0) return 0;
-    pause=strtol(str,&p,10);//преобразуем из строки в longint (до p) и присваем паузе
+    pause=strtol(str,&p,10);//преобразуем из строки в longint число; указателю последнего элемента присваиваем значение p, все число присваем паузе - время паузы
     sleep(pause);
-    strcpy(command,p+1);//копируем строку command и вставляем ее в место с указателем p+1
+    strcpy(command,p+1);//копируем в command все с p+1
     int i = strlen(command)-1;//i присваиваем длину строки command-1
-    while(command[i] != '/'){//пока символ строки не равен слэшу, уменьшаем длину i и читаем предыдущий символ
+    while(command[i] != '/')//пока символ строки не равен слэшу, уменьшаем длину i и читаем предыдущий символ
+    {
         i--;
     }
     char mc[20];//создаем массив
-    strncpy(mc, command, i)// (тоже самое что strcpy только с защиткой) скопировали в mc весь command начиная с i(т.е. со слэша). т.о. читаем только имя команды, ограниченное слэшами
+    strncpy(mc, command, i)// (тоже самое что strcpy только с защиткой) скопировали в mc весь command начиная с i(т.е. со слэша). т.о. читаем только имя команды, ограниченное слэшэм
 
-    if(execl(command, mc, NULL)<0){
+    if(execl(command, mc, NULL)<0)//если команду выполнить невозможно, то пишем что она фейлится//mc-то из строки command, что мы должны выполнить
+    {
         printf("Error - %d", errno);
         printf("%s%s%s\n","Program ",command," failed to load.");
-        exit(0);
+        exit(0);//выходим
     }
-    exit(-1);
+    exit(-1);//выходим
 }
-int main(){ 
+int main()
+{ 
     int file;
     char c,*str,*cur;
     ssize_t size;
-    if((file=open("Input.txt",O_RDONLY))<0){
+    if((file=open("Input.txt",O_RDONLY))<0)
+    {
     	    printf("%s\n","Can't open file!");
             exit(-1);
     }
@@ -52,9 +57,11 @@ int main(){
     size=1;
     while (size!=0){
         size=read(file,&c,1);
-	if((c == NextLine)||(size==0)){ 	    
+	if((c == NextLine)||(size==0))
+	{ 	    
             *cur=0;
-            if(strlen(str)>0){ 
+            if(strlen(str)>0)
+            { 
 		      useless(str);
             }
             cur=str;
@@ -64,7 +71,8 @@ int main(){
 		  cur++;
 	    }
 	}    
-    if(close(file)<0){
+    if(close(file)<0)
+    {
         printf("%s\n","Can't close file!");
 	    exit(-1);
     }
