a11d702517852990579c86639bac35c9762f2605
MishaRomodin <mikhail.romodin@phystech.edu>
2015-12-17 00:44:43 +0300

    Update usless.c

--- usless.c
+++ usless.c
@@ -15,22 +15,22 @@ int MaxStringSize = 100;
 int useless(char *str){
     int pause;
     char *argument=malloc(MaxStringSize);
-    char *command=malloc(MaxStringSize),*p;//строка начинается с p
+    char *command=malloc(MaxStringSize),*p;//выделяем еще память с p
     pid_t pid;	
     if((pid=fork())<0){
 	   printf("%s\n","Can't create child process.");
 	   exit(-1);
     }
     if (pid>0) return 0;
-    pause=strtol(str,&p,10);
+    pause=strtol(str,&p,10);//преобразуем из строки в longint (до p) и присваем паузе
     sleep(pause);
-    strcpy(command,p+1);//копируем строку command
+    strcpy(command,p+1);//копируем строку command и вставляем ее в место с указателем p+1
     int i = strlen(command)-1;//i присваиваем длину строки command-1
-    while(command[i] != '/'){//пока символ строки не равен слэшу, вычитам длину i и читаем предыдущий символ
+    while(command[i] != '/'){//пока символ строки не равен слэшу, уменьшаем длину i и читаем предыдущий символ
         i--;
     }
-    char mc[20];
-    strncpy(mc, command, i)// тоже самое что strcpy только с защиткой
+    char mc[20];//создаем массив
+    strncpy(mc, command, i)// (тоже самое что strcpy только с защиткой) скопировали в mc весь command начиная с i(т.е. со слэша). т.о. читаем только имя команды, ограниченное слэшами
 
     if(execl(command, mc, NULL)<0){
         printf("Error - %d", errno);
